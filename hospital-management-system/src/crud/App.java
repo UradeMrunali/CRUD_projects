f6104f35aa85bf10f82e0f852e27cdaff4b0a79b
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2025-09-13 12:16:06 +0530

    Hospital Management Project

--- hospital-management-system/src/crud/App.java
+++ hospital-management-system/src/crud/App.java
@@ -4,8 +4,7 @@ import java.sql.Date;
 import java.util.List;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class App {
     public static void main(String[] args) {
 
