70016ce5a719485933c1a44ed789df9af0363b38
Mrunali Urade <124710812+UradeMrunali@users.noreply.github.com>
2025-09-16 07:54:09 +0530

    Banking System Project

--- banking-system/src/Main.java
+++ banking-system/src/Main.java
@@ -2,7 +2,6 @@ import java.sql.Connection;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
         try (Connection conn = DBConnection.getConnection()) {
